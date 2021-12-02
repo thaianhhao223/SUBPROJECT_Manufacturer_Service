6a5f845c5cc704886025654e0824e79faf05fe7a
thaianhhao223 <thaianhhao322@gmail.com>
2021-12-02 14:03:22 +0700

    Hào hoàn thành Manufacturer_Service_Eureka_Client

--- src/main/java/com/sub_project/ManufacturerService/ManufacturerServiceApplication.java
+++ src/main/java/com/sub_project/ManufacturerService/ManufacturerServiceApplication.java
@@ -2,8 +2,10 @@ package com.sub_project.ManufacturerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ManufacturerServiceApplication {
 
 	public static void main(String[] args) {
